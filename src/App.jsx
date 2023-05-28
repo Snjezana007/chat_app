27c17d541d68eb4511fca3e614b8a7aaf75e4c4a
Snježana007 <120574058+Snjezana007@users.noreply.github.com>
2023-05-28 17:46:46 +0200

    "pokušaj promjena"

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import React, {useState, useEffect } from 'react';
+import Scaledrone from 'scaledrone-react';
+
+function App() {
+
+const [messages, setMessages] = useState ([]);
+const [messageInput, setMessageInput] =useState ('');
+
+const drone = new Scaledrone('ImB5jZWgMgCKsQPL');
+
+
+useEffect(
+  () => {
+    drone.on('message', (message)=>{
+      const newMessage = { author: messagemenber.clientData-name,text: message.data};
+      setMessages((prevMessages) => [...prevMessages, newMessages]);
+    });
+  }, []);
+
+  const handleInputChange = (event) => {
+    setMessageInput(event.target.value);
+  };
+
+  const handleSendmessage = () => {
+    if (messageInput.trim() !== '') {
+      drone.publish({ 
+        room: 'chat', 
+        message: messageInput.trim(),
+      });
+      setMessageInput('');
+    }
+  };
+
+
+  return (
+    <div className="App">
+      <div className="ChatWindow">
+        {
+          messages.map((message, index) => (
+            <div key={index} className="Message">
+              <span className="Author">{message.author}</span>
+              <span className="Text">{message.tekst}</span>
+              </div>
+          )
+          )
+        } 
+      </div>
+
+      <div className="InputContainer">
+<input type="text"
+className="MessageInput" 
+value={messageInput}
+onChange={handleInputChange}
+placeholder="Type your message..."/>
+
+<button className="SendButton" 
+onClick={handleSandMessage}> Send 
+</button>
+
+      </div>
+    </div>
+  );
+
+}
+export default App;
