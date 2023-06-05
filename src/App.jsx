bd1be979cbb14ed921d579932a0cf4aa72babb03
Snježana007 <120574058+Snjezana007@users.noreply.github.com>
2023-06-05 20:18:29 +0200

    u funkciju unesen dio za prikaz poruka i članova

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
-import React, {useState, useEffect } from 'react';
-import Scaledrone from 'scaledrone-react';
+import {useState, useEffect } from 'react';
+import "./App.css";
+import { Input, Messages, OnlineMembers } from "./components";
+import { randomColor, randomName } from "./RandomUserHelpers/RandomUser";
+
 
 function App() {
 
 const [messages, setMessages] = useState([]);
+const [member, setmember] = useState ({
+  username: randomName(),
+  color: randomColor(),
+});
+const [drone, setDrone] = useState(null);
+const [room, setRoom]= useState(null);
+const [onlineMembers, setOnlineMembers] = useState([]);
+
+useEffect(() => {
+  const drone = new window.Scaledrone(ImB5jZWgMgCKsQPL, {data: member,});
+drone.on("open", (error) => {
+  if (error) {
+    return console.error(error);
+  }
+  member.id = drone.clientId;
+  setmember(member);
+});
+const room = drone.subscribe("observable-room");
+room.on("message", (message) => {
+  setMessages((prevState)) => [...prevState, message]);
+
+});
+
+
+
+}
+)
+
 const [messageInput, setMessageInput] = useState('');
 
-const drone = new Scaledrone('ImB5jZWgMgCKsQPL');
+const drone = new setDrone('ImB5jZWgMgCKsQPL');
 
 
 useEffect(() => {
@@ -58,7 +89,7 @@ onChange={handleInputChange}
 placeholder="Type your message..."/>
 
 <button className="SendButton" 
-onClick={handleSandMessage}> Send 
+onClick={handleSendmessage}> Send 
 </button>
 
       </div>
