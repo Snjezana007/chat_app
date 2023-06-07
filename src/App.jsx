6191f6037aa7f584e0c828be5b3ec375ee4ce677
Snježana007 <snjeza.miksa@gmail.com>
2023-06-07 11:17:00 +0200

    ispravljene greške optimizirana funkcionalnost

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,70 @@
-import {useState, useEffect } from 'react';
+
+import React, { useState, useEffect } from 'react';
 import "./App.css";
-import { Input, Messages, OnlineMembers } from "../src/components";
+import { Input, Messages, OnlineMembers } from "./components";
 import { randomColor, randomName } from "./helpers/RandomUser";
 
-
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [member, setMember] = useState({
+    username: randomName(),
+    color: randomColor(),
+  });
+  const [drone, setDrone] = useState(null);
+  const [room, setRoom] = useState(null);
+  const [onlineMembers, setOnlineMembers] = useState([]);
 
-const [messages, setMessages] = useState([]);
-const [member, setMember] = useState ({
-  username: randomName(),
-  color: randomColor(),
-});
-const [drone, setDrone] = useState(null);
-const [room, setRoom]= useState(null);
-const [onlineMembers, setOnlineMembers] = useState([]);
-
-const sId = `${process.env.REACT_APP_CHANNEL_ID}`
-
-    this.drone = new window.Scaledrone(sId, {
-      data: this.state.member,
+  useEffect(() => {
+    const sId = process.env.REACT_APP_CHANNEL_ID;
+    const drone = new window.Scaledrone(sId, {
+      data: member,
     });
 
-drone.on("open", (error) => {
-  if (error) {
-    return console.error(error);
-  }
-  member.id = drone.clientId;
-  setMember(member);
-});
+    drone.on("open", (error) => {
+      if (error) {
+        return console.error(error);
+      }
+      const updatedMember = { ...member, id: drone.clientId };
+      setMember(updatedMember);
+    });
 
-const room = drone.subscribe("observable-room");
-room.on("message", (message) => {
-  setMessages((prevState)) => [...prevState, message];
-});
+    const room = drone.subscribe("observable-room");
+    room.on("message", (message) => {
+      setMessages((prevState) => [...prevState, message]);
+    });
 
+    room.on("members", (currentMembers) => {
+      setOnlineMembers([...currentMembers]);
+    });
 
-room.on("members", (currentMembers) => {
-  setOnlineMembers([...currentMembers]);
-});
+    room.on("member_join", (member) => {
+      setOnlineMembers((prevOnlineMembers) => [...prevOnlineMembers, member]);
+    });
 
-room.on("member_join", (member) => {
-  setOnlineMembers((prevOnlineMembers) => [...prevOnlineMembers, member]);
-});
+    room.on("member_leave", (memberLeft) => {
+      setOnlineMembers((prevMembers) =>
+        prevMembers.filter((m) => m.id !== memberLeft.id)
+      );
+    });
 
-room.on("member_leave", (memberLeft) => {
-  setOnlineMembers((prevMembers) =>
-    prevMembers.filter((member) => member.id !== memberLeft.id)
-  );
-});
+    setDrone(drone);
+  }, []);
 
-setDrone(drone);
-}, []);
-
-const onSendMessage = (message) => {
-drone.publish({
-  room: "observable-room",
-  message,
-});
-};
-
+  const onSendMessage = (message) => {
+    drone.publish({
+      room: "observable-room",
+      message,
+    });
+  };
 
   return (
     <div className="App">
-      
       <h1>Algebra Chat App</h1>
       <OnlineMembers onlineMembers={onlineMembers} currentMember={member} />
       <Messages messages={messages} currentMember={member} />
-      <Input onSendMessage={onSendMessage} />     </div>
+      <Input onSendMessage={onSendMessage} />
+    </div>
   );
 }
 
 export default App;
-
--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App/>);
-
-const script = document.createElement("script");
-script.src = "https://cdn.scaledrone.com/scaledrone.min.js";
-document.head.appendChild(script);
 
 
-reportWebVitals();
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
